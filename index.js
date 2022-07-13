4d110c4e1edd1effc6d29fd4e4064ab1db64b8b8
hnunezo <hector.nunez.oviedo@gmail.com>
2022-07-12 22:42:58 -0400

    xd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static("build"));
 
 app.use(cors());
 //JSON PARSER AGREGA BODY A LA REQUEST
